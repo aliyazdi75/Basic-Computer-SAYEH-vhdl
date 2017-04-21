3e1e54438b3fe1de42ea398e77e2795a0e7442d3
Ali Yazdi <ma.yazdi75@gmail.com>
2017-04-21 14:01:08 +0430

    merge

--- alu.vhd
+++ alu.vhd
@@ -19,7 +19,12 @@
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+<<<<<<< HEAD
+use IEEE.numeric_std.all;
+=======
 use ieee.std_logic_unsigned.all;
+use IEEE.NUMERIC_STD.ALL;
+>>>>>>> origin/master
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
@@ -53,8 +58,17 @@ end alu;
 
 architecture Behavioral of alu is
 
+COMPONENT ali16BFA
+    PORT(
+         a : IN  std_logic_vector(15 downto 0);
+         b : IN  std_logic_vector(15 downto 0);
+         cin : IN  std_logic;
+         cout : OUT  std_logic;
+         s : OUT  std_logic_vector(15 downto 0)
+        );
+END COMPONENT;
 
-component comprator16Bits is
+component comprator16Bits
    Port ( a : in  STD_LOGIC_VECTOR (15 downto 0);
            b : in  STD_LOGIC_VECTOR (15 downto 0);
            l : in  STD_LOGIC;
@@ -65,33 +79,63 @@ component comprator16Bits is
            gta : out  STD_LOGIC);
 end component comprator16Bits;
 
-	signal alu_and, alu_or, alu_src, alu_slc, alu_cmp, alu_xor : STD_LOGIC_VECTOR (15 downto 0);  
+<<<<<<< HEAD
+	signal co_add,co_sub,alu_cmp_eq, alu_cmp_gt, alu_cmp_lt : STD_LOGIC;
+	signal add, sub : STD_LOGIC_VECTOR (15 downto 0);
+=======
+component random_generator is
+    Port ( A : in  STD_LOGIC_VECTOR (15 downto 0);
+           B : in  STD_LOGIC_VECTOR (15 downto 0);
+           Output : out  STD_LOGIC_VECTOR (15 downto 0));
+end component random_generator;
+
+component two_complement is
+    Port ( A : in  STD_LOGIC_VECTOR (15 downto 0);
+           Output : out  STD_LOGIC_VECTOR (15 downto 0));
+end component two_complement;
+
+	signal alu_and, alu_or, alu_src, alu_slc, alu_cmp, alu_xor,alu_rand,alu_comp,alu_mul,alu_div : STD_LOGIC_VECTOR (15 downto 0);  
 	signal cout_add, cout_sub, alu_cmp_eq, alu_cmp_gt, alu_cmp_lt : STD_LOGIC;
 	signal add, sub : STD_LOGIC_VECTOR (16 downto 0);
+>>>>>>> origin/master
+	
+
 	
 begin
 	
-	alu_and <= a and b;
-	alu_or <= a or b;
-	alu_xor <= a xor b;
 	cmp: comprator16Bits port map(a,b,'0','1','0',alu_cmp_lt,alu_cmp_eq,alu_cmp_gt);
+<<<<<<< HEAD
+   adder: ali16BFA port map (a,b,cin,co_add,add);
+   subtractor: ali16BFA port map (a,not(b),cin,co_sub,sub);
+=======
+	
+	rand: random_generator port map(a,b,alu_rand); --not connected to alu_out!!!
+	complement: two_complement port map(b,alu_comp); --not connected to alu_out!!!
+
+	
    add <= (('0' & a) + ('0' & b) + cin);
-   sub <= (('0' & a) - ('0' & b) + cin);
+   sub <= (('0' & a) - ('0' & b) - cin);
+	
+	alu_mul <= std_logic_vector(unsigned(a(7 downto 0)) * unsigned(b(7 downto 0))); --not connected to alu_out!!! 
+	alu_div <= std_logic_vector(unsigned(a) / unsigned(b));  --not connected to alu_out!!!
+	
+	
+>>>>>>> origin/master
 	
 	SEQ: process(aandb,aorb,axorb,aaddb,asubb,amulb,acmpb,shrb,shlb,b15to0)
 	  begin
 		if(aandb='1')then
-			alu_out<=alu_and;
+			alu_out<=a and b;
 		elsif(aorb='1')then
-			alu_out<=alu_or;
+			alu_out<=a or b;
 		elsif(axorb='1')then
-			alu_out<=alu_xor;
+			alu_out<=a xor b;
 		elsif(aaddb='1')then
-			alu_out<=add(15 downto 0);
-			cout <= add(16);
+			alu_out<=add;
+			cout <= co_add;
 		elsif(asubb='1')then
-			alu_out<=sub(15 downto 0);
-			cout <= sub(16);
+			alu_out<=sub;
+			cout <= co_sub;
 		elsif(acmpb='1')then
 			cout <= alu_cmp_eq;
 			zout <= alu_cmp_lt;
--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,5 @@
--intstyle "ise" -incremental -lib "secureip" -o "D:/Ali Yazdi/Documents/GitHub/Architecture_Project/sayeh_PC_isim_beh.exe" -prj "D:/Ali Yazdi/Documents/GitHub/Architecture_Project/sayeh_PC_beh.prj" "work.sayeh_PC" 
+<<<<<<< HEAD
+-intstyle "ise" -incremental -lib "secureip" -o "D:/Ali Yazdi/Documents/GitHub/Architecture_Project/stb_isim_beh.exe" -prj "D:/Ali Yazdi/Documents/GitHub/Architecture_Project/stb_beh.prj" "work.stb" 
+=======
+-intstyle "ise" -incremental -lib "secureip" -o "C:/Users/Windows 10/Documents/GitHub/Architecture_Project/tc_tb_isim_beh.exe" -prj "C:/Users/Windows 10/Documents/GitHub/Architecture_Project/tc_tb_beh.prj" "work.tc_tb" 
+>>>>>>> origin/master
